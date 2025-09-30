7aa96b7f6deae67de637dfb1f1c834a046e42051
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-09-30 15:14:44 +0500

    round and abs function

---
+++ String Functions/number_functions.sql
@@ -0,0 +1,13 @@
+-- ROUND FUNCTION
+
+SELECT 3.516 AS random_number,
+ROUND(3.516, 2) AS round_number_1,
+ROUND(3.516, 1) AS round_number_2,
+ROUND(3.516, 0) AS round_number_3
+
+
+-- ABS FUNCTION
+
+SELECT -10,
+ABS(-10) AS abs_number_1,
+ABS(10) AS abs_number_2
