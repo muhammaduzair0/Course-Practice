008703be40b285f35239e8fa47b9728eb1c7a02a
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-06 13:35:42 +0500

    Show all orders that were placed during the month of February

--- Date & Time Functions/orders_of_the_years.sql
+++ Date & Time Functions/orders_of_the_years.sql
@@ -6,3 +6,18 @@ COUNT(*) AS NrOfOrders
 FROM Sales.Orders
 GROUP BY YEAR(OrderDate)
 
+-- Show all orders that were placed during the month of February
+
+SELECT 
+    OrderID,
+    DATENAME(month, OrderDate) AS OrderMonth
+FROM Sales.Orders
+WHERE MONTH(OrderDate) = 2
+
+
+SELECT
+DATENAME(MONTH, OrderDate) AS OrderMonth,
+COUNT(*) AS Total_order
+FROM Sales.Orders
+WHERE MONTH(OrderDate) = 2
+GROUP BY DATENAME(MONTH, OrderDate)
