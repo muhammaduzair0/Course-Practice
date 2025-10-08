18d4edb39f12ae59b53ce4106dc74eec8122849a
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-08 12:28:13 +0500

    Find the average shipping duration in days for each month With DateDiff Function

--- Date & Time Functions/datediff.sql
+++ Date & Time Functions/datediff.sql
@@ -14,3 +14,11 @@ BirthDate,
 DATEDIFF(year, BirthDate, GETDATE()) AS Age
 FROM Sales.Employees
 
+-- Find the average shipping duration in days for each month
+
+SELECT
+MONTH(OrderDate) AS OrderDate,  
+AVG(DATEDIFF(day, OrderDate, ShipDate)) AS AvgShipping
+FROM Sales.Orders
+GROUP BY MONTH(OrderDate)
+
