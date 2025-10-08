cc03b7c428cf4ac66b5ff29c4e94e965f8a22f31
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-08 12:29:43 +0500

    Show CreationTime using the following format:     DAY WED JAN Q1 2025 12:34:56 PM

--- Date & Time Functions/format.sql
+++ Date & Time Functions/format.sql
@@ -16,3 +16,17 @@ SELECT
 FROM Sales.Orders
 
 
+/* Show CreationTime using the following format:
+    DAY WED JAN Q1 2025 12:34:56 PM */
+
+SELECT
+    ORDERID,
+    CreationTime,
+    'Day ' + FORMAT(CreationTime, 'ddd MMM') + ' Q'+ DATENAME(QUARTER, CreationTime)+ ' ' + FORMAT(CreationTime, 'yyyy hh:mm:ss tt') AS CustomFormat
+FROM Sales.Orders
+
+SELECT
+    FORMAT(OrderDate, 'MMM yy') OrderDate,
+    COUNT(*) NumberOfOrders
+FROM Sales.Orders
+GROUP BY FORMAT(OrderDate, 'MMM yy')
