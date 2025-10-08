0517740e2fc86ce7d082e816a09793fbe8d9b9b0
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-08 12:28:28 +0500

    Time Gap Analysis With DateDiff Function

--- Date & Time Functions/datediff.sql
+++ Date & Time Functions/datediff.sql
@@ -22,3 +22,12 @@ AVG(DATEDIFF(day, OrderDate, ShipDate)) AS AvgShipping
 FROM Sales.Orders
 GROUP BY MONTH(OrderDate)
 
+-- Time Gap Analysis
+-- Find the number of days between each order and the previous order
+
+SELECT
+OrderID,
+OrderDate CurrentOrderDate,
+LAG(OrderDate) OVER (ORDER BY OrderDate) PreviousOrderDate,
+DATEDIFF(day, LAG(OrderDate) OVER (ORDER BY OrderDate), OrderDate) NumberOfDays
+FROM Sales.Orders
