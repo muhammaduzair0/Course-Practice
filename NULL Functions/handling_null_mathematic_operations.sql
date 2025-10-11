373421d152734605e4ae36e577e694c4bfbbae1d
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-11 14:15:34 +0500

    Null with Mathematic Operations

---
+++ NULL Functions/handling_null_mathematic_operations.sql
@@ -0,0 +1,21 @@
+/* Display the full name of customers in a single field 
+    by merging their first and last names
+    and add 10 bonus points to each customer's score. */
+
+SELECT
+CustomerID,
+FirstName,
+LastName,
+-- CONCAT(COALESCE(FirstName, ''), COALESCE(LastName, '')) AS FullName
+COALESCE(FirstName, '') + ' ' + COALESCE(LastName, '') AS FullName,
+Score,
+COALESCE(Score, 0) + 10 AS ScoreWithBonus
+FROM Sales.Customers
+
+-- SELECT
+-- CustomerID,
+-- FirstName,
+-- LastName,
+-- FirstName + ' ' + COALESCE(LastName, '') AS FullName,
+-- COALESCE(Score, 0) + 10 AS ScoreWithBonus
+-- FROM Sales.Customers
