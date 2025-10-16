f6e6cc2cd4057874a0726bc2f7fd8996469e93cf
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-16 16:07:58 +0500

    Window Function SUM, PARTITION BY and Data Frame for PRECIDING

--- Window Functions/total_sales.sql
+++ Window Functions/total_sales.sql
@@ -43,3 +43,12 @@ OrderStatus,
 Sales,
 SUM(Sales) OVER(PARTITION BY OrderStatus ORDER BY OrderDate ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING) AS TotalSales
 FROM Sales.Orders
+
+SELECT
+OrderID,
+OrderDate,
+OrderStatus,
+Sales,
+SUM(Sales) OVER(PARTITION BY OrderStatus ORDER BY OrderDate ROWS 2 PRECEDING) AS TotalSales -- Shortcut only for preceding
+FROM Sales.Orders
+
