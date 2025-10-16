bc0bf8dd5ae4ff7fe346f45019f9d4d8a6bd5059
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-16 16:07:05 +0500

    Window Function SUM, PARTITION AND DATA FRAME

--- Window Functions/total_sales.sql
+++ Window Functions/total_sales.sql
@@ -32,3 +32,14 @@ SELECT
     Sales,
     RANK() OVER(ORDER BY Sales DESC) RankSales
 FROM Sales.Orders
+
+SELECT *
+FROM Sales.Orders
+
+SELECT
+OrderID,
+OrderDate,
+OrderStatus,
+Sales,
+SUM(Sales) OVER(PARTITION BY OrderStatus ORDER BY OrderDate ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING) AS TotalSales
+FROM Sales.Orders
