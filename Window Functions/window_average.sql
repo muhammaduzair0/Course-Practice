66068c6b594c5c7ed29dbac3dc02951c886cfd23
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-21 11:33:12 +0500

    Window function, Find the average sales across all orders  and the average sales for each product, Additionally, provide details such as OrderID, order date

--- Window Functions/window_average.sql
+++ Window Functions/window_average.sql
@@ -12,3 +12,16 @@ SELECT
 -- AVG(COALESCE(Sales, 0)) OVER(PARTITION BY ProductID) AvgSales (HandlingNulls)
 FROM Sales.Orders
 
+/* Find the average sales across all orders 
+and the average sales for each product */
+-- Additionally, provide details such as OrderID, order date
+
+SELECT
+    OrderID,
+    OrderDate,
+    ProductID,
+    Sales,
+    AVG(Sales) Over() AvgSales,
+    AVG(Sales) Over(PARTITION BY ProductID) AvgSalesByProducts
+FROM Sales.Orders
+
