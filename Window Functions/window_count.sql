5057da84cab5208dce86be586e5eaf7bd63d7f7d
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-21 11:31:24 +0500

    Window function, Check whether the table 'Orders' contains any duplicate rows

--- Window Functions/window_count.sql
+++ Window Functions/window_count.sql
@@ -12,3 +12,10 @@ SELECT
     COUNT(*) OVER(PARTITION BY CustomerID) OrdersByCustomers
 FROM Sales.Orders
 
+-- Check whether the table 'Orders' contains any duplicate rows
+
+SELECT
+    OrderID,
+    COUNT(*) OVER(PARTITION BY OrderID) CheckPK
+FROM Sales.Orders
+
