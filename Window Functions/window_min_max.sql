f89fd4385b380c2aa39a0a22495bd7665fb17812
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-21 11:37:33 +0500

    Window function, Find the deviation of each sales from the minimum and maximum sales amount

--- Window Functions/window_min_max.sql
+++ Window Functions/window_min_max.sql
@@ -16,3 +16,29 @@ SELECT
     MAX(Sales) OVER(PARTITION BY ProductID) HighestSalesByProduct
 FROM Sales.Orders
 
+-- Find the deviation of each sales from the minimum and maximum sales amount
+SELECT
+    OrderID,
+    ProductID,
+    OrderDate,
+    Sales,
+    MIN(Sales) OVER() LowestSales,
+    MAX(Sales) OVER() HighestSales,
+    Sales - MIN(Sales) OVER() DeviationFromMin,
+    MAX(Sales) OVER() - Sales DeviationFromMax
+FROM Sales.Orders
+
+-- Show the employee who have the highest salaries
+
+SELECT *
+FROM Sales.Employees
+
+SELECT
+    *
+FROM(
+SELECT
+        *,
+        MAX(Salary) OVER() HighestSalary
+    FROM Sales.Employees
+)t
+WHERE Salary = HighestSalary
