645b339fafb3e88cec076b5148b381ec4552745c
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-21 11:33:56 +0500

    Window function, Find all orders where sales are higher than the average sales across all orders

--- Window Functions/window_average.sql
+++ Window Functions/window_average.sql
@@ -40,3 +40,14 @@ SELECT
     AVG(COALESCE(Score,0)) OVER() AvgScoreWithoutNull
 FROM Sales.Customers
 
+-- Find all orders where sales are higher than the average sales across all orders
+SELECT
+    *
+FROM(
+    SELECT
+        OrderID,
+        ProductID,
+        Sales,
+        AVG(Sales) OVER() AvgSales
+    FROM Sales.Orders
+)t WHERE Sales > AvgSales
