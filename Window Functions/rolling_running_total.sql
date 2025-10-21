88b95a36ec1edd82ee67db984c7680fb91fdf4e1
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-21 11:38:03 +0500

    Window function, Calculate the moving average of sales for each product over time, including the next order

--- Window Functions/rolling_running_total.sql
+++ Window Functions/rolling_running_total.sql
@@ -9,3 +9,13 @@ AVG(Sales) Over(PARTITION BY ProductID) AvgSalesByProduct,
 AVG(Sales) Over(PARTITION BY ProductID ORDER BY OrderDate) MovingAvg
 FROM Sales.Orders
 
+-- Calculate the moving average of sales for each product over time, including the next order
+
+SELECT
+OrderID,
+OrderDate,
+ProductID,
+Sales,
+AVG(Sales) Over(PARTITION BY ProductID) AvgSalesByProduct2,
+AVG(Sales) Over(PARTITION BY ProductID ORDER BY OrderDate ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING) RollingAvg
+FROM Sales.Orders
