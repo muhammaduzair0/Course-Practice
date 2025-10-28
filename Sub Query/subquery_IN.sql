dce4da3271f6a83a047577a43d4023a982c0c05e
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-28 17:23:59 +0500

    Subquery IN:  Show all customer details and find the total orders of each customers

--- Sub Query/subquery_IN.sql
+++ Sub Query/subquery_IN.sql
@@ -10,3 +10,21 @@ WHERE CustomerID IN (
     WHERE Country = 'Germany') -- Subquery
             
 
+-- Show the details of orders for customer who are not from Germany
+
+-- Main Query
+SELECT *
+FROM Sales.Orders
+WHERE CustomerID IN (
+    SELECT CustomerID
+    FROM Sales.Customers
+    WHERE Country != 'Germany'
+) -- Subquery
+-- Main Query
+SELECT *
+FROM Sales.Orders
+WHERE CustomerID NOT IN (
+    SELECT CustomerID
+    FROM Sales.Customers
+    WHERE Country = 'Germany'
+) -- Subquery
