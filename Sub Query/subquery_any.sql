91b970ade1ce4814c6a4e8ef7e423b3e74325d50
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-10-28 17:25:03 +0500

    Subquery ANY: Find female employees whose salaries are greater than the salaries of all male employees

--- Sub Query/subquery_any.sql
+++ Sub Query/subquery_any.sql
@@ -12,3 +12,16 @@ FROM Sales.Employees WHERE Gender = 'F'
 AND Salary > ANY (SELECT Salary FROM Sales.Employees WHERE Gender = 'M') -- Subquery
 
 SELECT FirstName, Salary FROM Sales.Employees WHERE Gender = 'M';
+
+-- Find female employees whose salaries are greater than the salaries of all male employees
+
+
+-- Main Query
+SELECT 
+EmployeeID,
+FirstName,
+Salary
+FROM Sales.Employees WHERE Gender = 'F'
+AND Salary > ALL (SELECT Salary FROM Sales.Employees WHERE Gender = 'M') -- Subquery
+
+SELECT FirstName, Salary FROM Sales.Employees WHERE Gender = 'M';
