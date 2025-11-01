c52dea0eb8696cc524f9e7241004302fec275fbe
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-01 15:03:56 +0500

    fixed

--- Common Table Expressions/multiple_standalone_cte.sql
+++ Common Table Expressions/multiple_standalone_cte.sql
@@ -11,7 +11,8 @@ WITH
         GROUP BY CustomerID
     ),
 
-    -- Step2: Find the last order date for each customer
+-- Step2: Find the last order date for each customer/
+
     CTE_Last_Order
     AS
     (
@@ -20,17 +21,16 @@ WITH
             MAX(OrderDate) AS Last_Order
         FROM Sales.Orders
         GROUP BY CustomerID
+    ),
     )
 
 -- Main Query
+
 SELECT
     c.CustomerID,
     c.FirstName,
     c.LastName,
     cts.TotalSales,
-    clo.Last_Order
 FROM Sales.Customers c
     LEFT JOIN CTE_TotalSales cts
     ON cts.CustomerID = c.CustomerID
-    LEFT JOIN  CTE_Last_Order clo
-    ON clo.CustomerID = c.CustomerID
