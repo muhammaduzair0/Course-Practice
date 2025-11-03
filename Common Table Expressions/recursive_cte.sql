26ab5f9703cd2b32ea211de02903ee8cbfdf5581
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-03 15:41:41 +0500

    Recursive CTE Series Of Numbers from 1 to 20

---
+++ Common Table Expressions/recursive_cte.sql
@@ -0,0 +1,16 @@
+-- Generate a Sequence of Numbers from 1 to 20
+
+WITH
+    Series
+    AS
+    (
+        -- Anchor Query
+        SELECT
+            1 AS MyNumber
+            UNION ALL
+            -- Recursive Query
+            SELECT
+            MyNumber + 1
+            FROM Series
+            WHERE MyNumber < 20
+    )
