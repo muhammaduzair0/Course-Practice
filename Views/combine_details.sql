0cedb48bfe4dbd6e091959df72255a6471f71f6b
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-05 16:42:02 +0500

    View Of Sales Order Details With Combining the table

---
+++ Views/combine_details.sql
@@ -0,0 +1,23 @@
+-- TASK: Provide view that combines details from orders, products, customers, and employees
+
+
+CREATE VIEW Sales.V_Order_Details AS (
+    SELECT 
+    o.OrderID,
+    o.OrderDate,
+    p.Product,
+    p.Category,
+    COALESCE(c.FirstName, '') + ' ' + COALESCE(c.LastName, '') CustomerName,
+    c.Country CustomerCountry,
+    COALESCE(e.FirstName, '') + ' ' + COALESCE(e.LastName, '') SalesName,
+    e.Department,
+    o.Sales,
+    o.Quantity
+    FROM Sales.Orders AS o
+    LEFT JOIN Sales.Products AS p
+    ON p.ProductID = o.ProductID
+    LEFT JOIN Sales.Customers AS c   
+    ON c.CustomerID = o.CustomerID
+    LEFT JOIN Sales.Employees AS e
+    ON e.EmployeeID = o.SalesPersonID
+)
