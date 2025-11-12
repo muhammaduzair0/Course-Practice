ef5480a5c7734c6223bc9e1efd2ffa79b6b5e588
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-12 16:02:16 +0500

    Define Steps

---
+++ Stored Procedures/total_number_customer.sql
@@ -0,0 +1,6 @@
+-- Step 1: Write a Query
+
+-- For US Customers Find the Total Number of Customers and the Average Score.
+-- Define Stored Procedure
+-- Step:2 Turning the Query Into a Stored Procedure
+
