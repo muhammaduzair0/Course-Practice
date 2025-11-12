2ceb509d17f7f40d2bf2dcdd517e8e65c72d27d4
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-12 16:03:02 +0500

    GetCustomerSummary With Default Value

--- Stored Procedures/total_number_customer.sql
+++ Stored Procedures/total_number_customer.sql
@@ -4,3 +4,13 @@
 -- Define Stored Procedure
 -- Step:2 Turning the Query Into a Stored Procedure
 
+ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA' -- Default Value
+AS
+BEGIN
+SELECT
+    COUNT(*) AS Total_Customers,
+    AVG(Score)AS Avg_Score
+FROM Sales.Customers
+WHERE Country = @Country;
+
+END
