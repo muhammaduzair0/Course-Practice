3c212bbc52e513722961266f865f8575b590dc1a
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-13 14:57:52 +0500

    Some Fixes

--- Stored Procedures/total_number_customer.sql
+++ Stored Procedures/total_number_customer.sql
@@ -4,24 +4,12 @@
 -- Define Stored Procedure
 -- Step:2 Turning the Query Into a Stored Procedure
 
+-- Total Customers from Germany: 2
+-- Average Score from Germany: 425
+
 ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA' -- Default Value
 AS
 BEGIN
-SELECT
-    COUNT(*) AS Total_Customers,
-    AVG(Score)AS Avg_Score
-FROM Sales.Customers
-WHERE Country = @Country;
-
--- Find the total Nr. Of Orders and Total Sales
-
-SELECT
-COUNT(OrderID) TotalOrders,
-SUM(Sales) TotalSales
-FROM Sales.Orders o
-JOIN Sales.Customers c
-ON c.CustomerID = o.CustomerID
-WHERE Country = @Country;
 
 END
 
