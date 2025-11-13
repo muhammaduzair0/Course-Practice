bcb414c8a59ebae9e543d297d170be3354d7dfa5
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-13 15:01:10 +0500

    Insert The Data For Checking Employee Logs

---
+++ Stored Procedures/insertingData.sql
@@ -0,0 +1,7 @@
+
+SELECT * FROM Sales.EmployeesLogs
+
+INSERT INTO Sales.Employees
+VALUES
+-- (6, 'Maria', 'Doe', 'HR', '1988-01-12', 'F', 80000, 3)
+(7, 'Maria', 'Doe', 'HR', '1988-01-12', 'F', 80000, 3)
