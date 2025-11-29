615aef99291d6752244faa2abdf0a54d5020b93e
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-29 15:07:37 +0500

    Select table, index, avg fragmentation, page count FROM sys.dm_db

---
+++ Indexes/Fragmentations.sql
@@ -0,0 +1,6 @@
+SELECT 
+    tbl.name AS TableName,
+    idx.name AS IndexName,
+    s.avg_fragmentation_in_percent,
+    s.page_count
+FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS s
