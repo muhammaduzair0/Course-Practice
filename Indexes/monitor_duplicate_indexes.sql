14f48917f849ca6831490d34c939ed53da2c5f6e
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-29 15:35:27 +0500

    Monitor Duplicate Indexes: window Function COUNT and Partition By table name, column name as Column Count From System Indexes

--- Indexes/monitor_duplicate_indexes.sql
+++ Indexes/monitor_duplicate_indexes.sql
@@ -3,3 +3,5 @@ SELECT
     col.name AS IndexColumn,
     idx.name AS IndexName,
     idx.type_desc AS IndexType,
+    COUNT(*) OVER(PARTITION BY tbl.name, col.name) ColumnCount
+FROM sys.indexes idx
