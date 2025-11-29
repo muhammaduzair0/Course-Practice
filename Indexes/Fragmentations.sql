8fb18217b077d2c820d5083b65bd4fb83b961d35
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-29 15:08:58 +0500

    INNER JOIN sys tables ON system object id with table object id,

--- Indexes/Fragmentations.sql
+++ Indexes/Fragmentations.sql
@@ -4,3 +4,5 @@ SELECT
     s.avg_fragmentation_in_percent,
     s.page_count
 FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS s
+INNER JOIN sys.tables tbl
+ON s.object_id = tbl.object_id
