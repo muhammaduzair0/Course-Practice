ff7ff1e903b24519bd1296f25991dc824d6be135
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-11-29 15:36:09 +0500

    Monitor Duplicate Indexes: JOIN system table, index, columns, sorted by Column Count

--- Indexes/monitor_duplicate_indexes.sql
+++ Indexes/monitor_duplicate_indexes.sql
@@ -5,3 +5,7 @@ SELECT
     idx.type_desc AS IndexType,
     COUNT(*) OVER(PARTITION BY tbl.name, col.name) ColumnCount
 FROM sys.indexes idx
+JOIN sys.tables tbl ON idx.object_id = tbl.object_id
+JOIN sys.index_columns ic ON ic.object_id = ic.object_id AND idx.index_id = ic.index_id
+JOIN sys.columns col ON ic.object_id = col.object_id AND ic.column_id = col.column_id
+ORDER BY ColumnCount ASC
