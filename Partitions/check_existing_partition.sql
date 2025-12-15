67612fbd0c2af8cc098b058f3140f5c2bb004fbf
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-12-15 15:08:58 +0500

    Query lists all existing Filegroup, LogicalFileName, PhysicalName, FileSize

--- Partitions/check_existing_partition.sql
+++ Partitions/check_existing_partition.sql
@@ -14,3 +14,18 @@ SELECT *
 FROM sys.filegroups
 WHERE type = 'FG'
 
+-- Query lists all existing Filegroup, LogicalFileName, PhysicalName, FileSize
+
+SELECT
+    fg.name AS FilegroupName,
+    mf.name AS LogicalFileName,
+    mf.physical_name AS PhysicalFilePath,
+    mf.size / 128 AS SizeInMB
+FROM
+    sys.filegroups fg
+JOIN
+    sys.master_files mf ON fg.data_space_id = mf.data_space_id
+WHERE
+    mf.database_id = DB_ID('SalesDB');
+
+
