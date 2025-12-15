e1ccc46abdfa06828c56684a5a9d2028ec62de00
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-12-15 15:08:15 +0500

    Update Statistics

---
+++ Indexes/update_statistics.sql
@@ -0,0 +1,16 @@
+SELECT
+    SCHEMA_NAME(t.schema_id) AS SchemaName,
+    t.name AS TableName,
+    s.name AS StatisticName,
+    sp.last_updated AS LastUpdate,
+    DATEDIFF(day, sp.last_updated, GETDATE()) AS LastUpdateDay,
+    sp.rows AS 'Rows',
+    sp.modification_counter AS ModificationsSinceLastUpdate
+FROM sys.stats AS s
+JOIN sys.tables t
+ON s.object_id = t.object_id
+CROSS APPLY sys.dm_db_stats_properties(s.object_id, s.stats_id) AS sp
+ORDER BY sp.modification_counter DESC;
+
+UPDATE STATISTICS Sales.DBCustomers _WA_Sys_00000003_3A4CA8FD
+UPDATE STATISTICS Sales.DBCustomers
